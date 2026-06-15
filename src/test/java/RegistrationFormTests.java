8977ea4062e4d67e1ecad4abbed4d06782d7eb56
Yana Pirogova <pirogova.yana@gmail.com>
2026-06-16 00:51:36 +0300

    Сделаны исправления по комментариям: 1) Папка main удалена. 2) Добавила executeJavaScript(""" document.getElementById('fixedban')?.remove(); document.querySelector('footer')?.remove(); """); 3) Переделан выбор пола и хобби. 4) Использован метод uploadFromClasspath -> $("#uploadPicture").uploadFromClasspath("picture.png"); 5) Добавлены проверки в таблице.

--- src/test/java/RegistrationFormTests.java
+++ src/test/java/RegistrationFormTests.java
@@ -1,11 +1,8 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormTests extends TestsBase {
@@ -13,29 +10,40 @@ public class RegistrationFormTests extends TestsBase {
       @Test
       void successAllRegistrationFormFieldsPopulation() {
 
-        File myFile = new File("src/test/resources/my_png.png");
-
         open("/automation-practice-form");
+        executeJavaScript("""
+                                 document.getElementById('fixedban')?.remove();
+                                 document.querySelector('footer')?.remove();
+                           """);
         $("input[id=firstName]").setValue("Jane");
         $("input[id=lastName]").setValue("Smith");
         $("input[id=userEmail]").setValue("janesmith@mail.com");
-        $("input[value=Female]").click();
+        $("[id=genterWrapper]").find(byText("Female")).click();
         $("input[id=userNumber]").setValue("8909678545");
         $("input[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption("March");
         $("[class=react-datepicker__year-select]").selectOptionByValue("1990");
         $x("//div[text()='15']").click();
         $("input[id=subjectsInput]").setValue("Test Subject");
-        $("input[id=hobbies-checkbox-3]").click();
-        $("input[id=uploadPicture]").uploadFile(myFile);
+        $("[id=hobbiesWrapper]").find(byText("Music")).click();
+        $("input[id=uploadPicture]").uploadFromClasspath("my_png.png");
         $("[id=currentAddress]").setValue("Current Address");
-        executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
         $("[id=state]").click();
         $("input[id=react-select-3-input]").setValue("Haryana").pressEnter();
         $("input[id=react-select-4-input]").setValue("Karnal").pressEnter();
         $("button[id=submit]").click();
 
         $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Student Name Jane Smith"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Student Email janesmith@mail.com"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Gender Female"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Mobile 8909678545"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Date of Birth 15 March,1990"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Subjects Test Subject"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Hobbies Music"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Picture my_png.png"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Address Current Address"));
+        $x("//div[@class='table-responsive']").shouldHave(text("State and City Haryana Karnal"));
 
     }
 
@@ -43,14 +51,20 @@ public class RegistrationFormTests extends TestsBase {
       void requiredFieldsFormFieldsPopulation() {
 
         open("/automation-practice-form");
+        executeJavaScript("""
+                                 document.getElementById('fixedban')?.remove();
+                                 document.querySelector('footer')?.remove();
+                           """);
         $("input[id=firstName]").setValue("Rob");
         $("input[id=lastName]").setValue("Robin");
-        $("input[value=Male]").click();
+        $("[id=genterWrapper]").find(byText("Male")).click();
         $("input[id=userNumber]").setValue("8909678545");
-        executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
         $("button[id=submit]").click();
 
         $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Student Name Rob Robin"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Gender Male"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Mobile 8909678545"));
 
       }
 
@@ -58,7 +72,10 @@ public class RegistrationFormTests extends TestsBase {
       void submittingEmptyFormFields() {
 
         open("/automation-practice-form");
-        executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
+        executeJavaScript("""
+                                 document.getElementById('fixedban')?.remove();
+                                 document.querySelector('footer')?.remove();
+                           """);
         $("button[id=submit]").click();
 
         $("[id=example-modal-sizes-title-lg]").shouldNotBe(visible);
@@ -69,16 +86,19 @@ public class RegistrationFormTests extends TestsBase {
       void submittIngncorrectEmailFormat() {
 
         open("/automation-practice-form");
+        executeJavaScript("""
+                                 document.getElementById('fixedban')?.remove();
+                                 document.querySelector('footer')?.remove();
+                           """);
         $("input[id=firstName]").setValue("Jane");
         $("input[id=lastName]").setValue("Smith");
         $("input[id=userEmail]").setValue("почта");
-        $("input[value=Female]").click();
+        $("[id=genterWrapper]").find(byText("Female")).click();
         $("input[id=userNumber]").setValue("8909678545");
         $("input[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption("March");
         $("[class=react-datepicker__year-select]").selectOptionByValue("1990");
         $x("//div[text()='15']").click();
-        executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
         $("button[id=submit]").click();
 
         $("[id=example-modal-sizes-title-lg]").shouldNotBe(visible);
